e980b3c19f0645c4f1b4068e6ce69cb2de35d2ac
Alexandre Carvalho <alexbh@gmail.com>
2023-01-29 17:40:34 +0000

    initial

---
+++ models/dayRegistry.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CONFIG = require('../config/config');
+
+const dayRegistrySchema = new Schema({
+    patient: String,
+    registryDate: Date,
+    bath: String,
+    bloodPressure: String,
+    temperature: String,
+    dayClassification: String,
+    description: String
+});
+
+module.exports = global.mongoConnection.model(CONFIG.mongodb.collections.dayRegistry, dayRegistrySchema);
